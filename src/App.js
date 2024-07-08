1b73d16ed93e2e5228f3a3b134c7d3a1ecad2987
Ben McLean <169668286+ben-at-catalyst@users.noreply.github.com>
2024-07-08 14:32:55 -0400

    Got package working.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Code } from "@ben-at-catalyst/ui";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Code>Package is working!</Code>
       </header>
     </div>
   );
